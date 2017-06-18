99827d161bcee70cbba42df22054941e9ba817b0
buonzz <buonzz@gmail.com>
2017-06-19 04:09:33 +0800

    rename project files

--- BzLoans/frmMain.Designer.cs
+++ BzLoans/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BzLoans
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(777, 449);
+            this.IsMdiContainer = true;
+            this.Name = "frmMain";
+            this.Text = "Buonzz Loans System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
