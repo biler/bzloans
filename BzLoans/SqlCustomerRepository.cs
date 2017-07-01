977f5c92f6ce40977c56e15aacc57985668e748d
buonzz <buonzz@gmail.com>
2017-07-02 06:21:17 +0800

    #3 implemented all

--- BzLoans/SqlCustomerRepository.cs
+++ BzLoans/SqlCustomerRepository.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace BzLoans
 {
     class SqlCustomerRepository : RepositoryInterface
     {
+        public List<Customer> All() {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM main.customers", Program.Connection);
+
+            List<Customer> output  = new List<Customer>();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Customer customer = new Customer();
+                customer.name = (String) reader[0];
+                output.Add(customer);
+            }
+            reader.Close();
+            return output;
+        }
     }
 }
