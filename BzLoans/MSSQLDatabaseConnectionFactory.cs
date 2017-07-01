c60a1abe711e2f412a9229aedbca9f05f8913f2b
buonzz <buonzz@gmail.com>
2017-07-02 05:58:13 +0800

    solves #2

---
+++ BzLoans/MSSQLDatabaseConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+
+namespace BzLoans
+{
+    class MSSQLDatabaseConnectionFactory
+    {
+        public SqlConnection create() {
+            String cs = "Persist Security Info = False; User ID = " +
+                Program.settings.db_username +
+                "; Password = " +
+                Program.settings.db_username +
+                "; Initial Catalog = " +
+                Program.settings.db_name +
+                "; Server = " +
+                Program.settings.db_host;
+
+            SqlConnection cn = new SqlConnection(cs);
+            return cn;
+        } // create
+    }
+}
--- BzLoans/Program.cs
+++ BzLoans/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SimpleInjector;
+using System.Data.SqlClient;
 
 namespace BzLoans
 {
@@ -11,6 +12,7 @@ namespace BzLoans
     {
         public static Container container;
         public static Settings settings;
+        public static SqlConnection Connection;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -20,6 +22,7 @@ namespace BzLoans
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Bootstrap();
+            ConnectToDB();
             Application.Run(new frmMain());
         }
 
@@ -32,5 +35,20 @@ namespace BzLoans
             container.Register<RepositoryInterface, SqlCustomerRepository>();
             container.Verify();
         }
+
+        private static void ConnectToDB() {
+
+            MSSQLDatabaseConnectionFactory factory = new MSSQLDatabaseConnectionFactory();
+            Connection = factory.create();
+
+            try
+            {
+                Connection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+}
     }
 }
