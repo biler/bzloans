f4afc5010fc210efd6501ff280c7599758b8ff35
buonzz <buonzz@gmail.com>
2017-07-02 23:23:31 +0800

    completed fields

--- BzLoans/SqlCustomerProfileRepository.cs
+++ BzLoans/SqlCustomerProfileRepository.cs
@@ -21,7 +21,24 @@ namespace BzLoans
             while (reader.Read())
             {
                 CustomerProfile customer = new CustomerProfile();
-                customer.Id = (Int32)reader[0];
+
+                customer.Id = (Int32)reader["id"];
+                customer.FirstName = reader["first_name"].ToString();
+                customer.MiddleName = reader["middle_name"].ToString();
+                customer.LastName = reader["last_name"].ToString();
+                customer.Title = reader["title"].ToString();
+                customer.Birthdate = (DateTime) reader["birthdate"];
+                customer.CustomerId = (Int32) reader["customer_id"];
+                customer.Address1 = reader["address1"].ToString();
+                customer.Address2 = reader["address2"].ToString();
+                customer.City = reader["city"].ToString();
+                customer.State = reader["state"].ToString();
+                customer.Country = reader["country"].ToString();
+                customer.Zip = (Int32)reader["zip"];
+                customer.Email = reader["email"].ToString();
+                customer.Phone = reader["phone_number"].ToString();
+                customer.Telephone = reader["tel_no"].ToString();
+
                 output.Add(customer);
             }
             reader.Close();
