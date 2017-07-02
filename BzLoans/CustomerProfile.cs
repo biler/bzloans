b885c30bec6ca7f2d7c3bb8e525b92b4a481a067
buonzz <buonzz@gmail.com>
2017-07-02 22:24:14 +0800

    profile repository

---
+++ BzLoans/CustomerProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BzLoans
+{
+    class CustomerProfile
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String MiddleName { get; set; }
+        public String LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public String Title { get; set; }
+        public Int32 CustomerId { get; set; }
+        public String Address1 { get; set; }
+        public String Address2 { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Country { get; set; }
+        public Int32 Zip { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public String Telephone { get; set; }
+    }
+}
---
+++ BzLoans/SqlCustomerProfileRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BzLoans
+{
+    class SqlCustomerProfileRepository : RepositoryInterface
+    {
+        public List<CustomerProfile> All()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM main.customer_profiles", Program.Connection);
+
+            List<CustomerProfile> output = new List<CustomerProfile>();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                CustomerProfile customer = new CustomerProfile();
+                customer.Id = (Int32)reader[0];
+                output.Add(customer);
+            }
+            reader.Close();
+            return output;
+        }
+    }
+}
